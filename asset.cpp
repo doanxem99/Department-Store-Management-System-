08c123cde401690d96b87d33ff154e01c9d3e705
nnhan727 <nhannguyen2772005@gmail.com>
2024-08-28 20:15:46 +0700

    Update asset.cpp

--- asset.cpp
+++ asset.cpp
@@ -4,5 +4,4 @@
 // set up the initial value for the static variable
 Tree<PersonnelManagement::Personnel> Asset::personnelTree;
 std::vector<ProductManagement::Product> Asset::productList;
-std::vector<std::string> Asset::historyOfChanges;
 int64_t Asset::pTotalMoney = 0;
