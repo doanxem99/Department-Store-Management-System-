c88376b6f2503941c82a61a28402c6e061e271d2
nnhan727 <nhannguyen2772005@gmail.com>
2024-08-28 20:15:23 +0700

    Update asset.h

--- asset.h
+++ asset.h
@@ -12,25 +12,7 @@ class Asset {
  public:
     static Tree<PersonnelManagement::Personnel> personnelTree;
     static std::vector<ProductManagement::Product> productList;
-
-    // If there is any change in an app, add it to historyOfChanges
-    static std::vector<std::string> historyOfChanges;
-
     static int64_t pTotalMoney;
-
-
-    // Import and export data from and to a file
-    // for backup when the system is down
-    bool exportProduct();
-    bool importProduct();
-
-    bool exportPersonnel();
-    bool importPersonnel();
-
-    bool exportHistoryOfChanges();
-    bool importHistoryOfChanges();
 };
-
-
 
 #endif  // ASSET_H_
